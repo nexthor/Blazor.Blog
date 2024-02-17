68e5dd1fda351e124dfdb0666609302394dd6db6
Nestor Danilo Mendoza Martínez <nestor.mendoza@outlook.com>
2024-02-17 10:15:00 -0600

    Add project files.

---
+++ Blazor.Blog.Common/Models/Post.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blazor.Blog.Common.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public int CategoryId { get; set; }
+        public string? Title { get; set; }
+        public string? Excerpt { get; set; }
+        public string? FeaturedImage { get; set; }
+        public string? Content { get; set; }
+        public DateTime? PublishedAt { get; set; } = DateTime.Now;
+    }
+}
